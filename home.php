fdb61e0167bba190185e915c49105d0d5f7a86a7
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-08-27 18:30:51 +0300

    improved code purity

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
-<?php 
-/* 
+<?php
+/*
 Template Name: home
 */
 ?>
@@ -7,7 +7,7 @@ Template Name: home
 <?php get_header();?>
 
     <main class="main">
-        <div class="inner">            
+        <div class="inner">
             <div class="main__grid">
                 <div class="row row_big">
                     <?php display_latest_posts(3, 0, 6); ?>
