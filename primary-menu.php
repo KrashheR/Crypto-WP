fc83b59a295ebcfa7c2e17d0c5e05a586a91f502
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-08-28 13:05:31 +0300

    removed useless code

--- primary-menu.php
+++ primary-menu.php
@@ -1,14 +1,4 @@
 <?php
-$post_id = get_the_ID();
-$categories = get_the_category($post_id);
 
-foreach ($categories as $category) {
-    $category_class = '';
-
-    if (is_category($category->term_id) || in_category($category->term_id)) {
-        $category_class = 'current-menu-item';
-    }
-    echo '<li class="' . $category_class . '"><a href="' . esc_url(home_url('/')) . '">' . $category->name . '</a></li>';
-}
 ?>
 
